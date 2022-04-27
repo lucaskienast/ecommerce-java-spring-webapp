d0a697d558f91fbaba8afcf75031619e20ac4d04
LucasKienast <lucas.kienast@hotmail.de>
2022-04-27 19:31:49 +0200

    initial commit

---
+++ src/main/java/com/kienast/ecommercespringbootpaypalwebapp/EcommerceSpringBootPaypalWebappApplication.java
@@ -0,0 +1,13 @@
+package com.kienast.ecommercespringbootpaypalwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringBootPaypalWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringBootPaypalWebappApplication.class, args);
+	}
+
+}
